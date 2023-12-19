088d706e9f39d2eb5efaceb10a8bdd4f5aed705b
Henry Walton <hwfwalton@gmail.com>
2023-12-18 22:02:51 -0800

    Fix weird issue slots on non-visible pages were still available

--- BookView/book_view_clue_slot_collider.gd
+++ BookView/book_view_clue_slot_collider.gd
@@ -4,9 +4,17 @@ signal clue_submitted(found_clue_item: FoundClueItem)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if not is_visible_in_tree():
+		_on_hidden()
 
 
 func emitFoundClueSubmitted(found_clue_item: FoundClueItem):
 	clue_submitted.emit(found_clue_item)
 
+func _on_hidden():
+	# When not visible, move the slot to a different collision layer so the
+	# draggable clue items don't detect this slot
+	collision_layer = 2
+
+func _on_draw():
+	collision_layer = 1
