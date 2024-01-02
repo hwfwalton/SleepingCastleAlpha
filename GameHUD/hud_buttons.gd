4774de1f454891d5b75b042c6847b46eaef38c8f
Henry Walton <hwfwalton@gmail.com>
2024-01-01 22:56:59 -0800

    Hide the options button when Options is open

--- GameHUD/hud_buttons.gd
+++ GameHUD/hud_buttons.gd
@@ -5,12 +5,21 @@ extends MarginContainer
 # Book Icon
 # https://www.freepik.com/icon/book_828370
 
+@onready var options_view_scene = preload("res://options_menu.tscn")
+@onready var options_button_node = $HUDButtons/OptionsButton
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 func _on_options_button_pressed():
-	var options = load("res://options_menu.tscn").instantiate()
+	var options_view = options_view_scene.instantiate()
 	var scene_to_add_to = get_tree().current_scene.get_viewport()
-	scene_to_add_to.add_child(options)
+	scene_to_add_to.add_child(options_view)
+	options_button_node.visible = false
+	options_view.tree_exited.connect(_on_options_view_closed)
+
+func _on_options_view_closed():
+	options_button_node.visible = true
