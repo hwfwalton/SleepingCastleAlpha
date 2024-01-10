5441211c88af092d9fa51f16231a07f3735c4a48
Henry Walton <hwfwalton@gmail.com>
2024-01-10 00:34:23 -0800

    Drop unused export var from ArtifactViewTemplate

--- clue_artifacts/ArtifactViewTemplate.gd
+++ clue_artifacts/ArtifactViewTemplate.gd
@@ -1,7 +1,5 @@
 extends Control
 
-@export var container_artifacts_group: String = ""
-
 func getClueNodes():
 	var clue_nodes = get_children().filter(func(node):
 		return node.is_in_group("clue_item")
