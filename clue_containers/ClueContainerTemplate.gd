de6a6087e10987ecbd5922b3c52b8120dc1b0721
Henry Walton <hwfwalton@gmail.com>
2024-01-10 00:35:59 -0800

    Drop unused ready function

--- clue_containers/ClueContainerTemplate.gd
+++ clue_containers/ClueContainerTemplate.gd
@@ -13,8 +13,3 @@ func getArtifactNodes():
 		return node.is_in_group("artifact_sparkle")
 	)
 	return artifact_sparkle
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
